42360e8020d3f077a818e7110d55c5233de2f35a
Sayed-Hasinoor-Alam <sayedhasinooralam.info@gmail.com>
2023-02-10 20:42:25 +0530

    sayed - ui one complete

---
+++ lcotodo/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return(
+    <div>
+    <h1>Hello React</h1>
+    <p>coding</p>
+    </div>
+  );
+
+}
+
+export default App;
